81000a622c3a55177375600a354b88b4d340d456
environmentallyconsciouscoder <environmentallyconsciouscoder@gmail.com>
2024-09-08 22:56:11 +0100

    feat: added type for form and recommendations data from api

---
+++ packages/frontend/src/types/recommendationsTypes.ts
@@ -0,0 +1,12 @@
+export interface ClothingItem {
+    type: string;
+    brand: string;
+    color: string;
+    size?: string;
+    price: string;
+    link: string;
+  }
+  export interface RecommendationsResponse {
+    style: ClothingItem[];
+    description: string;
+  }
