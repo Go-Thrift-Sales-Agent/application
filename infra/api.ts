d143eac80dca6bb4aae96d04ea9f67b9c9bc7ca8
environmentallyconsciouscoder <environmentallyconsciouscoder@gmail.com>
2024-09-08 21:39:27 +0100

    feat: create api endpoint for ai recommendation

--- infra/api.ts
+++ infra/api.ts
@@ -1,8 +1,9 @@
-import { bucket } from "./storage";
-
-export const api = new sst.aws.ApiGatewayV2("Api");
-
-api.route("GET /", {
-  link: [bucket],
-  handler: "packages/functions/src/api.handler",
+export const api = new sst.aws.ApiGatewayV2("Api", {
+  transform: {
+    route: {
+    }
+  },
+  cors: true
 });
+
+api.route("GET /ai/recommendations", "packages/functions/src/recommendations.main");
